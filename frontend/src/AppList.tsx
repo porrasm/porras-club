def538742284f35516d7487d209bd8edb5159cfb
Eetu Ikonen <eetu.e.ikonen@gmail.com>
2022-11-23 00:53:44 +0200

    Asd

--- frontend/src/AppList.tsx
+++ frontend/src/AppList.tsx
@@ -5,25 +5,37 @@ type SApp = {
   name: string
   redirect?: string
   component?: () => JSX.Element
+  action?: () => void
 }
 
-const apps: SApp[] = [
-  {
-    name: "streaming service (better than discord)",
-    redirect: "http://video.porras.club"
-  },
-  {
-    name: "infinite loop",
-    component: () => <InfiniteLoop />
-  },
-  {
-    name: "calculator",
-    component: () => <Calculator />
-  }
-]
+const verFlip: React.CSSProperties = {
+  transform: "scaleY(-1)"
+}
+
+
+
 
 export const AppList = () => {
+  const [apps, setApps] = useState<SApp[]>([
+    {
+      name: "streaming service (better than discord)",
+      redirect: "http://video.porras.club"
+    },
+    {
+      name: "infinite loop",
+      component: () => <InfiniteLoop />
+    },
+    {
+      name: "calculator",
+      component: () => <Calculator />
+    },
+    {
+      name: "austria mode (for view in new zeland)",
+      action: () => setFlip(!flip)
+    }
+  ])
   const [currentApp, setCurrentApp] = useState<SApp>()
+  const [flip, setFlip] = useState(false)
 
   const selectApp = (a: string) => {
     const newApp = apps.find(app => a === app.name)
@@ -33,15 +45,20 @@ export const AppList = () => {
       location.href = newApp.redirect
     }
 
+    if (newApp?.action) {
+      newApp.action()
+    }
   }
 
-  return <div style={{ "display": "flex", "flexDirection": "column" }}>
-    {"<br\\>"}
-    <span>apps list {"(server functionelity); "}</span>
-    {"<br\\>"}
-    {currentApp?.component ? currentApp.component() : apps.map(a => <button onClick={() => selectApp(a.name)}>{a.name}</button>)}
-    {"<br\\>"}
-    {currentApp ? <button onClick={() => setCurrentApp(undefined)}>go backk</button> : <span>select app from list above</span>}
-    {"<br\\>"}
+  return <div style={flip ? verFlip : {}}>
+    <div style={{ "display": "flex", "flexDirection": "column" }}>
+      {"<br\\>"}
+      <span>apps list {"(server functionelity); "}</span>
+      {"<br\\>"}
+      {currentApp?.component ? currentApp.component() : apps.map(a => <button onClick={() => selectApp(a.name)}>{a.name}</button>)}
+      {"<br\\>"}
+      {currentApp ? <button onClick={() => setCurrentApp(undefined)}>go backk</button> : <span>select app from list above</span>}
+      {"<br\\>"}
+    </div>
   </div>
 }
